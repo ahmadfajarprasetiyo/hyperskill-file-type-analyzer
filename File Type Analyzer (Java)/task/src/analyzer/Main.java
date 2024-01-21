4e2a31ee967a992fa0d739437b565143c87af2c1
Ahmad Fajar Prasetiyo <me@ahmadfajar.com>
2024-01-21 20:41:17 +0700

    Stage 1/5: Is this a PDF?

--- File Type Analyzer (Java)/task/src/analyzer/Main.java
+++ File Type Analyzer (Java)/task/src/analyzer/Main.java
@@ -1,7 +1,30 @@
 package analyzer;
 
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        String inputFile = args[0];
+        String patternFile = args[1];
+        String outputString = args[2];
+
+        try {
+
+            String allBytes = Files.readString(Paths.get(inputFile));
+
+            if (allBytes.contains(patternFile)) {
+                System.out.println(outputString);
+            } else {
+                System.out.println("Unknown file type");
+            }
+
+        } catch (IOException ex) {
+            System.out.println(ex.toString());
+        }
     }
 }
